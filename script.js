197e8575d3334d68d1c3360d7b9c6a4a15c68268
Vaishnavi <tyagivaishno@gmail.com>
2025-11-25 00:20:38 +0530

    Added input field for entering city name and JavaScript handling

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    const city = document.getElementById('city-input').value;
+    console.log('City entered:', city);
+
+    // Additional code to fetch weather data will be added later
+});
